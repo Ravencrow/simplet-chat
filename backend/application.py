c97b0b54187c63715af9f2bb50185a491e5115c3
Juan Lucha <juanlucha@gmail.com>
2019-06-10 22:51:39 +0200

    Added socket logic to backend plus changed frontend to Vue project

--- backend/application.py
+++ backend/application.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, send, join_room, rooms
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
@@ -11,3 +11,21 @@ socketio = SocketIO(app)
 @app.route("/")
 def index():
     return "Project 2: TODO"
+
+
+@socketio.on('message')
+def handleMessage(message):
+    print('Message: ' + message)
+    send(message, broadcast=True)
+
+
+@socketio.on('create')
+def createRoom(room):
+    print('Creating room: ' + room)
+    join_room(room)
+    roomList = rooms()
+    emit('room-list', roomList)
+
+
+if __name__ == '__main__':
+    socketio.run(app)
--- frontend/src/app-state/actions.js
+++
@@ -1,6 +0,0 @@
-export const actions = {
-  registerUser: (store, user) => {
-    store.setState({ user: user })
-    console.log(store)
-  }
-}
